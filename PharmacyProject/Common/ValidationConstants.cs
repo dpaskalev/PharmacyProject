6abe494a0d0fb7888c40a1b13ec2a6d71249351e
Daniel <danielpaskalev14@Gmail.com>
2025-07-24 16:17:48 +0300

    Create folder "Common" & static class "ValidationConstraints"

---
+++ PharmacyProject/Common/ValidationConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace PharmacyProject.Common
+{
+    public static class ValidationConstants
+    {
+        public const byte MedicineNameMinLenght = 3;
+        public const byte MedicineNameMaxLenght = 250;
+        public const double MedicinePriceMinValue = 0;
+        public const double MedicinePriceMaxValue = 10000;
+        public const byte MedicineDescriptionMinValue = 0;
+        public const int MedicineDescriptionMaxValue = 10000;
+
+        public const byte MedicineTypeNameMinLenght = 0;
+        public const byte MedicineTypeNameMaxLenght = 250;
+
+        public const byte PharmacyNameMinlenght = 0;
+        public const int PharmacyNameMaxLebnght = 1000;
+        public const byte PharmacyLocationMinLenght = 0;
+        public const int PharmacyLocationMaxLenght = 1000;
+    }
+}
