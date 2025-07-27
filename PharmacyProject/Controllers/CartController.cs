7e4cb609519706ea799285212166b3c12f899877
Daniel <danielpaskalev14@Gmail.com>
2025-07-27 17:09:22 +0300

    Create "CartController", "CartViewModel" & function "Index"

---
+++ PharmacyProject/Controllers/CartController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PharmacyProject.Data;
+using PharmacyProject.Data.DataModels;
+using PharmacyProject.Servises;
+using PharmacyProject.Servises.Interfaces;
+using PharmacyProject.VewModels;
+using System;
+
+namespace PharmacyProject.Controllers
+{
+    [Authorize]
+    public class CartController : BaseController
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(UserManager<IdentityUser> userManager, ICartService cartService)
+            :base(userManager)
+        {
+            _cartService = cartService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = await _cartService.GetIndexAsync(GetUserId());
+
+            return View(model);
+        }
+
+
+    }
+}
---
+++ PharmacyProject/VewModels/CartViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmacyProject.VewModels
+{
+    public class CartViewModel
+    {
+        [Required]
+        public int MedicineId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public DateTime ExperationDate { get; set; }
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string TypeName { get; set; } = null!;
+
+        public string? ImageURL { get; set; }
+    }
+}
