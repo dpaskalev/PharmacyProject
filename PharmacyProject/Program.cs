1cff9136b8f67a0aed65636de2084fc3d45834a6
Daniel <danielpaskalev14@Gmail.com>
2025-07-28 14:45:31 +0300

    Fix "Login" & "Register" scafolded items

--- PharmacyProject/Program.cs
+++ PharmacyProject/Program.cs
@@ -19,7 +19,14 @@ namespace PharmacyProject
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             builder.Services.AddScoped<IMedicineService, MedicineService>();
